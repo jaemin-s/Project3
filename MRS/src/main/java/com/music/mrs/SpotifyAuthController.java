9fbd83733fdeabcf5c64d814d3c574e47119b8e1
changsoon123 <sool0458@naver.com>
2023-05-30 15:27:02 +0900

    로그인 완료

--- MRS/src/main/java/com/music/mrs/SpotifyAuthController.java
+++ MRS/src/main/java/com/music/mrs/SpotifyAuthController.java
@@ -1,6 +1,10 @@
 package com.music.mrs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +17,10 @@ import se.michaelthelin.spotify.requests.authorization.authorization_code.Author
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
 import java.net.URI;
 
 
@@ -22,89 +29,119 @@ import java.net.URI;
 //requestAccessToken() 메서드를 통해 사용자 인증 및 액세스 토큰을 얻는 작업을 수행합니다.
 //이 클래스는 독립적으로 작동하며, 액세스 토큰을 요청하고 얻은 결과를 콘솔에 출력하는 역할을 합니다.
 //주로 테스트, 개발 단계에서 사용되며, 얻은 액세스 토큰을 다른 곳에 전달하거나 저장할 필요가 있을 때 활용할 수 있습니다.
-	
+
 @Controller
 @SessionAttributes("spotifyApi")
 public class SpotifyAuthController {
 
-    @Value("${client.id}")
-    private String clientId;
+	@Value("${client.id}")
+	private String clientId;
 
-    @Value("${redirect.uri}")
-    private String redirectUri;
+	@Value("${client.secret}")
+	private String clientSecret;
 
-    private SpotifyApi spotifyApi;
+	@Value("${redirect.uri}")
+	private String redirectUri;
 
-    @PostConstruct
-    public void initialize() {
-        // SpotifyApi 객체를 생성하고 클라이언트 ID, 리디렉션 URI를 설정합니다.
-        spotifyApi = new SpotifyApi.Builder()
-                .setClientId(clientId)
-                .setRedirectUri(URI.create(redirectUri))
-                .build();
-    }
+	private SpotifyApi spotifyApi;
 
-    @GetMapping("/login")
-    public String login(HttpSession session) {
-        // 사용자 인증을 위한 인증 URL을 생성합니다.
-        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-                .scope("user-read-private,user-read-email")
-                .show_dialog(true)
-                .build();
+	@PostConstruct
+	public void initialize() {
+		// SpotifyApi 객체를 생성하고 클라이언트 ID, 리디렉션 URI를 설정합니다.
+		spotifyApi = new SpotifyApi.Builder()
+				.setClientId(clientId)
+				.setClientSecret(clientSecret)
+				.setRedirectUri(URI.create(redirectUri))
+				.build();
+	}
 
-        String authorizationUrl = authorizationCodeUriRequest.execute().toString();
+	@GetMapping("/login")
+	public String login(HttpSession session) {
+		// 사용자 인증을 위한 인증 URL을 생성합니다.
+		AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+				.scope("user-read-private,user-read-email")
+				.show_dialog(true)
+				.state("some-state-value")
+				.build();
 
-        // 세션에 SpotifyApi 객체를 저장합니다.
-        session.setAttribute("spotifyApi", spotifyApi);
+		String authorizationUrl = authorizationCodeUriRequest.execute().toString();
 
-        // 생성된 인증 URL로 리다이렉트합니다.
-        return "redirect:" + authorizationUrl;
-    }
+		// 세션에 SpotifyApi 객체를 저장합니다.
+		session.setAttribute("spotifyApi", spotifyApi);
 
-    @GetMapping("/callback")
-    public ModelAndView callback(@RequestParam("code") String code,
-                                 @RequestParam("state") String state,
-                                 HttpSession session) {
-        // 세션에서 SpotifyApi 객체를 가져옵니다.
-        SpotifyApi spotifyApi = (SpotifyApi) session.getAttribute("spotifyApi");
+		// 생성된 인증 URL로 리다이렉트합니다.
+		return "redirect:" + authorizationUrl;
+	}
 
-        try {
-            // 액세스 토큰 요청을 생성하고 실행합니다.
-            AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
-            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+	@GetMapping("/callback")
+	public ModelAndView callback(@RequestParam("code") String code,
+			@RequestParam("state") String state,
+			HttpSession session) {
+		// 세션에서 SpotifyApi 객체를 가져옵니다.
+		SpotifyApi spotifyApi = (SpotifyApi) session.getAttribute("spotifyApi");
 
-            // 받은 액세스 토큰을 처리합니다.
-            String accessToken = authorizationCodeCredentials.getAccessToken();
-            String refreshToken = authorizationCodeCredentials.getRefreshToken();
-            int expiresIn = authorizationCodeCredentials.getExpiresIn();
+		try {
+			// 액세스 토큰 요청을 생성하고 실행합니다.
+			AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+			System.out.println(authorizationCodeRequest);
 
-            // TODO: 액세스 토큰을 저장하고 필요한 처리를 수행합니다.
+			AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+			System.out.println(authorizationCodeCredentials);
 
-            // 처리 결과를 ModelAndView에 담아 리다이렉트합니다.
-            ModelAndView modelAndView = new ModelAndView("redirect:/success");
-            modelAndView.addObject("message", "Authentication successful!");
-            return modelAndView;
-        } catch (Exception e) {
-            // 예외 처리
-            ModelAndView modelAndView = new ModelAndView("redirect:/error");
-            modelAndView.addObject("message", "Authentication failed!");
-            return modelAndView;
-        }
-    }
 
-    // 인증 성공 시 처리할 엔드포인트
-    @GetMapping("/success")
-    public ModelAndView success(@RequestParam("message") String message) {
-        ModelAndView modelAndView = new ModelAndView("success");
-        modelAndView.addObject("message", message);
-        return modelAndView;
-    }
 
-    // 인증 실패 시 처리할 엔드포인트
-    @GetMapping("/error")
-    public ModelAndView error(@RequestParam("message") String message) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("message", message);
-        return modelAndView;
-    }
+
+			// 받은 액세스 토큰을 처리합니다.
+			String accessToken = authorizationCodeCredentials.getAccessToken();
+			System.out.println(accessToken);
+			String refreshToken = authorizationCodeCredentials.getRefreshToken();
+			System.out.println(refreshToken);
+			int expiresIn = authorizationCodeCredentials.getExpiresIn();
+			System.out.println(expiresIn);
+
+			// TODO: 액세스 토큰을 저장하고 필요한 처리를 수행합니다.
+
+			// 사용자 정보를 세션에 저장합니다.
+            session.setAttribute("accessToken", accessToken);
+            session.setAttribute("refreshToken", refreshToken);
+            session.setAttribute("expiresIn", expiresIn);
+			
+			// 처리 결과를 ModelAndView에 담아 리다이렉트합니다.
+			ModelAndView modelAndView = new ModelAndView("redirect:/success");
+			modelAndView.addObject("message", "Authentication successful!");
+			return modelAndView;
+		} catch (Exception e) {
+			// 예외 처리
+			ModelAndView modelAndView = new ModelAndView("redirect:/error");
+			modelAndView.addObject("message", "Authentication failed!");
+			return modelAndView;
+		}
+	}
+
+	@GetMapping("/resources/img/Spotify_Icon_RGB_Black.png")
+	public ResponseEntity<Resource> getImageResource(HttpServletRequest request) throws IOException {
+		Resource resource = new ClassPathResource("/resources/img/Spotify_Icon_RGB_Black.png");
+
+		if (resource.exists()) {
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	// 인증 성공 시 처리할 엔드포인트
+	@GetMapping("/success")
+	public ModelAndView success(@RequestParam("message") String message) {
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("message", message);
+		return modelAndView;
+	}
+
+	// 인증 실패 시 처리할 엔드포인트
+	@GetMapping("/error")
+	public ModelAndView error(@RequestParam("message") String message) {
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("message", message);
+		return modelAndView;
+	}
 }
--- MRS/src/main/webapp/WEB-INF/views/home.jsp
+++ MRS/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,44 +1,51 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-	
-	<%@ include file="include/header.jsp" %>
-	<link rel="alternate" type="application/json+oembed" href="https://open.spotify.com/oembed?url=https://open.spotify.com/album/4ghBzVOTFoeKPPmyNKjVtI" />
-	
-	<section>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-		<h1>여기는 section</h1>
-		<table>
-			<thead>
-				<tr>
-					<td>기준 일자</td>
-					<td>기준 시각</td>
-					<td>하늘 형태</td>
-					<td>강수 형태</td>
-					<td>기온</td>
-				</tr>
-			</thead>
-			<tbody>
-				<tr id="resultRow">
-					<td>test</td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-			</tbody>
-		</table>
+<%@ include file="include/header.jsp"%>
+<link rel="alternate" type="application/json+oembed"
+	href="https://open.spotify.com/oembed?url=https://open.spotify.com/album/4ghBzVOTFoeKPPmyNKjVtI" />
 
-		<button id="playBtn" >재생</button>
-		<!-- <iframe id="player" type="text/html" width="200" height="200" src="https://www.youtube.com/embed/0-q1KafFCLU"></iframe> --> 
-		<div id="player"></div>
-  
+<section>
 
-      
-	</section>
-    
-    <%@ include file="include/footer.jsp"%>
-	<script>
-	
+	<a href="/mrs/login" class="spotify-login-button">
+  <img src="${pageContext.request.contextPath}/img/Spotify_Icon_RGB_Black.png" alt="Spotify Logo" class="spotify-logo">
+  <span class="login-text">로그인</span>
+</a>
+<p>${message}</p>
+
+	<h1>여기는 section</h1>
+	<table>
+		<thead>
+			<tr>
+				<td>기준 일자</td>
+				<td>기준 시각</td>
+				<td>하늘 형태</td>
+				<td>강수 형태</td>
+				<td>기온</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr id="resultRow">
+				<td>test</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+
+	<button id="playBtn">재생</button>
+	<!-- <iframe id="player" type="text/html" width="200" height="200" src="https://www.youtube.com/embed/0-q1KafFCLU"></iframe> -->
+	<div id="player"></div>
+
+
+
+</section>
+
+<%@ include file="include/footer.jsp"%>
+<script>
 	//공공데이터 날씨
 	/* window.onload = () => {
 			
@@ -89,67 +96,63 @@
 
 		} */
 
-		const $playBtn = document.getElementById('playBtn');
-		let vId = "0-q1KafFCLU";
-		$playBtn.addEventListener('click',function(){
-			console.log('클릭');
-			let state = player.getPlayerState();
-			console.log(state);
-			if(state == -1||state == 2){
-				player.playVideo();
-				console.log("재생");
-			}else if(state == 5){
-				console.log(player.getCurrentTime());
-				console.log(player.getDuration());
-				player.playVideo();
-			}else if(state == 1){
-				player.pauseVideo();
-				console.log("일시정지");
+	const $playBtn = document.getElementById('playBtn');
+	let vId = "0-q1KafFCLU";
+	$playBtn.addEventListener('click', function() {
+		console.log('클릭');
+		let state = player.getPlayerState();
+		console.log(state);
+		if (state == -1 || state == 2) {
+			player.playVideo();
+			console.log("재생");
+		} else if (state == 5) {
+			console.log(player.getCurrentTime());
+			console.log(player.getDuration());
+			player.playVideo();
+		} else if (state == 1) {
+			player.pauseVideo();
+			console.log("일시정지");
+		}
+	});
+
+	// 2. This code loads the IFrame Player API code asynchronously.
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
+	var player;
+	function onYouTubeIframeAPIReady() {
+		player = new YT.Player('player', {
+			height : '100',
+			width : '100',
+			videoId : vId,
+			events : {
+				'onReady' : onPlayerReady,
+				'onStateChange' : onPlayerStateChange
 			}
 		});
-		
-		// 2. This code loads the IFrame Player API code asynchronously.
-	      var tag = document.createElement('script');
+	}
 
-	      tag.src = "https://www.youtube.com/iframe_api";
-	      var firstScriptTag = document.getElementsByTagName('script')[0];
-	      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	// 4. The API will call this function when the video player is ready.
+	function onPlayerReady(event) {
+		event.target.playVideo();
+	}
 
-	      // 3. This function creates an <iframe> (and YouTube player)
-	      //    after the API code downloads.
-	      var player;
-	      function onYouTubeIframeAPIReady() {
-	        player = new YT.Player('player', {
-	          height: '100',
-	          width: '100',
-	          videoId: vId,
-	          events: {
-	            'onReady': onPlayerReady,
-	            'onStateChange': onPlayerStateChange
-	          }
-	        });
-	      }
-
-	      // 4. The API will call this function when the video player is ready.
-	      function onPlayerReady(event) {
-	        event.target.playVideo();
-	      }
-
-	      // 5. The API calls this function when the player's state changes.
-	      //    The function indicates that when playing a video (state=1),
-	      //    the player should play for six seconds and then stop.
-	      var done = false;
-	      function onPlayerStateChange(event) {
-	        if (event.data == YT.PlayerState.PLAYING && !done) {
-	          setTimeout(stopVideo, 6000);
-	          done = true;
-	        }
-	      }
-	      function stopVideo() {
-	        player.stopVideo();
-	      }
-	      
-	      
-	      
- 
-	</script>
+	// 5. The API calls this function when the player's state changes.
+	//    The function indicates that when playing a video (state=1),
+	//    the player should play for six seconds and then stop.
+	var done = false;
+	function onPlayerStateChange(event) {
+		if (event.data == YT.PlayerState.PLAYING && !done) {
+			setTimeout(stopVideo, 6000);
+			done = true;
+		}
+	}
+	function stopVideo() {
+		player.stopVideo();
+	}
+</script>
