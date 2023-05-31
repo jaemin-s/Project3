f32e4c209afc9e2295e0b014938e229200786c60
JeongEY321 <kingship89@naver.com>
2023-05-31 18:26:41 +0900

    회원정보 받아오기 및, 받아와서 사용하는거, 세션 저장 및 삭제 완료

--- MRS/src/main/java/com/music/mrs/SpotifyAuthController.java
+++ MRS/src/main/java/com/music/mrs/SpotifyAuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
@@ -29,7 +30,7 @@ import java.net.URI;
 //requestAccessToken() 메서드를 통해 사용자 인증 및 액세스 토큰을 얻는 작업을 수행합니다.
 //이 클래스는 독립적으로 작동하며, 액세스 토큰을 요청하고 얻은 결과를 콘솔에 출력하는 역할을 합니다.
 //주로 테스트, 개발 단계에서 사용되며, 얻은 액세스 토큰을 다른 곳에 전달하거나 저장할 필요가 있을 때 활용할 수 있습니다.
-
+@Slf4j
 @Controller
 @SessionAttributes("spotifyApi")
 public class SpotifyAuthController {
@@ -65,58 +66,68 @@ public class SpotifyAuthController {
 				.build();
 
 		String authorizationUrl = authorizationCodeUriRequest.execute().toString();
-
 		// 세션에 SpotifyApi 객체를 저장합니다.
 		session.setAttribute("spotifyApi", spotifyApi);
 
 		// 생성된 인증 URL로 리다이렉트합니다.
+		log.info("dd: "+authorizationUrl);
+		
 		return "redirect:" + authorizationUrl;
+	}
+	
+	@GetMapping("/logOut")
+	public String logOut(HttpSession session) {
+		session.invalidate();
+		return "redirect: http://localhost/mrs/";
 	}
 
 	@GetMapping("/callback")
-	public ModelAndView callback(@RequestParam("code") String code,
-			@RequestParam("state") String state,
-			HttpSession session) {
-		// 세션에서 SpotifyApi 객체를 가져옵니다.
-		SpotifyApi spotifyApi = (SpotifyApi) session.getAttribute("spotifyApi");
+    public ModelAndView callback(@RequestParam("code") String code,
+            @RequestParam("state") String state,
+            HttpSession session) {
+        // 세션에서 SpotifyApi 객체를 가져옵니다.
+        SpotifyApi spotifyApi = (SpotifyApi) session.getAttribute("spotifyApi");
+        log.info("spotifyApi: " + spotifyApi);
 
-		try {
-			// 액세스 토큰 요청을 생성하고 실행합니다.
-			AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
-			System.out.println(authorizationCodeRequest);
+        try {
+            // 액세스 토큰 요청을 생성하고 실행합니다.
+            AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+//            System.out.println(authorizationCodeRequest);
 
-			AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
-			System.out.println(authorizationCodeCredentials);
-
-
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+//            System.out.println(authorizationCodeCredentials);
 
 
-			// 받은 액세스 토큰을 처리합니다.
-			String accessToken = authorizationCodeCredentials.getAccessToken();
-			System.out.println(accessToken);
-			String refreshToken = authorizationCodeCredentials.getRefreshToken();
-			System.out.println(refreshToken);
-			int expiresIn = authorizationCodeCredentials.getExpiresIn();
-			System.out.println(expiresIn);
 
-			// TODO: 액세스 토큰을 저장하고 필요한 처리를 수행합니다.
 
-			// 사용자 정보를 세션에 저장합니다.
+            // 받은 액세스 토큰을 처리합니다.
+            String accessToken = authorizationCodeCredentials.getAccessToken();
+            System.out.println(accessToken);
+            log.info(accessToken);
+            String refreshToken = authorizationCodeCredentials.getRefreshToken();
+//            System.out.println(refreshToken);
+            int expiresIn = authorizationCodeCredentials.getExpiresIn();
+//            System.out.println(expiresIn);
+
+            // TODO: 액세스 토큰을 저장하고 필요한 처리를 수행합니다.
+
+            // 사용자 정보를 세션에 저장합니다.
             session.setAttribute("accessToken", accessToken);
             session.setAttribute("refreshToken", refreshToken);
             session.setAttribute("expiresIn", expiresIn);
-			
-			// 처리 결과를 ModelAndView에 담아 리다이렉트합니다.
-			ModelAndView modelAndView = new ModelAndView("redirect:/success");
-			modelAndView.addObject("message", "Authentication successful!");
-			return modelAndView;
-		} catch (Exception e) {
-			// 예외 처리
-			ModelAndView modelAndView = new ModelAndView("redirect:/error");
-			modelAndView.addObject("message", "Authentication failed!");
-			return modelAndView;
-		}
-	}
+            
+            // 처리 결과를 ModelAndView에 담아 리다이렉트합니다.
+            ModelAndView modelAndView = new ModelAndView("redirect:/success");
+            modelAndView.addObject("message", "Authentication successful!");
+            return modelAndView;
+        } catch (Exception e) {
+            // 예외 처리
+            e.printStackTrace();
+            ModelAndView modelAndView = new ModelAndView("redirect:/error");
+            modelAndView.addObject("message", "Authentication failed!");
+            return modelAndView;
+        }
+    }
 
 	@GetMapping("/resources/img/Spotify_Icon_RGB_Black.png")
 	public ResponseEntity<Resource> getImageResource(HttpServletRequest request) throws IOException {
@@ -144,4 +155,6 @@ public class SpotifyAuthController {
 		modelAndView.addObject("message", message);
 		return modelAndView;
 	}
+	
+	
 }
--- MRS/src/main/webapp/WEB-INF/views/include/side.jsp
+++ MRS/src/main/webapp/WEB-INF/views/include/side.jsp
@@ -21,6 +21,7 @@
 	src="${pageContext.request.contextPath }/js/commons.js" defer></script>
 
 </head>
+
 <body>
 
 	<!-- 왼쪽 고정 menu, controller -->
@@ -29,20 +30,21 @@
 		<!-- 페이지 넘어갈 곳 -->
 		<div class="menu">
 			<!-- 로그아웃상태에선 로그인, 회원가입 넣기 -->
-			<c:if test="${login == null}">
+			<c:if test="${accessToken == null}">
 				<div>
 					<a href="/mrs/login" class="spotify-login-button">
 						<img src="${pageContext.request.contextPath}/img/Spotify_Logo_RGB_Black.png" alt="Spotify Logo" class="spotify-logo"> 
 						<span class="login-text">로그인</span>
 					</a>
-					<p>${message}</p>
+					<p>${message} ${accessToken}</p>
 					<h3 id="test_text"></h3>
 				</div>
 			</c:if>
 			<!-- 로그인 되어있으면 환영인사. -->
-			<c:if test="${login != null}">
-				<h1>닉네임 님</h1>
-				<h3>환영합니다.</h3>
+			<c:if test="${accessToken != null}">
+				<h1></h1>
+				<h3 class="testCl">환영합니다.</h3>
+				<a href="/mrs/logOut" class="logOut">log out</a>
 			</c:if>
 
 			<a class="mainBtn" href="#">main page</a>
@@ -59,19 +61,42 @@
 </html>
 
 <script>
+
+const $testCl = document.querySelector(".testCl");
+
+window.onload = function() {
+	let display_name = '';
+	let display_email = '';
+	/* 닉네임 받아와서 사용 */
+		fetch('https://api.spotify.com/v1/me',
+				{
+			headers : {"Authorization" : `Bearer ${accessToken}`}
+			}).then(res => res.json())
+			.then(data => {
+				console.log(data);
+				console.log(data.display_name);
+				console.log(data.email);
+				display_name = data.display_name;
+				display_email = data.email;
+				document.querySelector('.menu>h1').textContent = display_name + ' 님';
+			});
+	console.log(display_name);
+}
+
+	
 	// 에어팟 클릭시 이미지 변경 및 컨트롤러 변경${pageContext.request.contextPath }
 
 	var $airImg = document.getElementById("airImg");
 
-	airImg.onclick = function() {
+	$airImg.onclick = function() {
 
-		if (airImg.getAttribute('src') === "${pageContext.request.contextPath}/img/air.png") {
+		if ($airImg.getAttribute('src') === "${pageContext.request.contextPath}/img/air.png") {
 			console.log('열렸따!');
-			airImg.setAttribute('src',
+			$airImg.setAttribute('src',
 					"${pageContext.request.contextPath}/img/air2.png");
-		} else if (airImg.getAttribute('src') === "${pageContext.request.contextPath}/img/air2.png") {
+		} else if ($airImg.getAttribute('src') === "${pageContext.request.contextPath}/img/air2.png") {
 			console.log('닫혔따!');
-			airImg.src = "${pageContext.request.contextPath}/img/air.png";
+			$airImg.src = "${pageContext.request.contextPath}/img/air.png";
 		}
 
 	}
