b15254da82009c0449fb223dc68b923bf2e7f471
origin <wndfla456@naver.com>
2023-06-03 20:24:34 +0900

    reply mapper 수정

--- MRS/src/main/java/com/music/mrs/ReplyController.java
+++ MRS/src/main/java/com/music/mrs/ReplyController.java
@@ -3,12 +3,14 @@ package com.music.mrs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.music.mrs.command.ReplyVO;
 import com.music.mrs.reply.service.IReplyService;
 
 @RestController
+@RequestMapping("/reply")
 public class ReplyController {
 
 	@Autowired
